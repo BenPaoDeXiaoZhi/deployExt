4cda2f9699855e8f166e086c9a2c0be81256b1e0
BenPaoDeXiaoZhi <mengfanzhi1232022@163.com>
2026-07-11 13:49:07 +0800

    v0.2.4: 添加警告

--- src/resolveProjectUrl.ts
+++ src/resolveProjectUrl.ts
@@ -1,10 +1,14 @@
+import { error } from "@actions/core";
 import { communityWeb } from "@ccw-api/api";
 export async function getUserFolderAndSb3MD5(projectOid: string) {
-  const { latestProjectLink } = await communityWeb.getCreationDetail(
-    projectOid,
-    "",
-    "EDIT",
-  );
+  const { latestProjectLink, isTeamwork } =
+    await communityWeb.getCreationDetail(projectOid, "", "EDIT");
+  if (isTeamwork) {
+    error(
+      `[CCW Extension Deploy] TODO: Teamwork project is not supported yet!`,
+    );
+    throw new Error("teamwork not supported yet");
+  }
   const userFolder = latestProjectLink.split("/").at(-2);
   const [sb3MD5] = latestProjectLink.split("/").at(-1).split(".");
   return { userFolder, sb3MD5 };
